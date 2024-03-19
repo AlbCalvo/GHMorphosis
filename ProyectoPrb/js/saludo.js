0bec763dcbb4748123eeecf0bc7c5799e055e18c
“Alberto <acmmin@gmail.com>
2024-03-19 08:32:29 +0100

    7

---
+++ ProyectoPrb/js/saludo.js
@@ -0,0 +1,10 @@
+var nombre = "Lope de Vega 3";
+var edad = 20;
+var presenteONo = true;
+var apellido = "Martín";
+var estatura = 1.70;
+
+
+console.log("Nombre" + nombre + typeof nombre);
+console.log("Edad" + edad + typeof edad);
+//alert("la pantalla es de " + nombre);
